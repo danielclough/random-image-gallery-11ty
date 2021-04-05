8d2748b2d464c3caaeb0c31774a4f6d0320085cc
danielclough <danielclough@gmail.com>
2021-04-05 03:44:17 -0700

    update output dir

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 const CleanCSS = require("clean-css");
 module.exports = function(eleventyConfig) {
+    dir: {
+        output: "docs"
+    },
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
